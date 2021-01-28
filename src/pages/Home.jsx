edc18f3b9a7eb177c13a526bebe8bbf25c18bfad
cuneydbolukoglu <cuneydbolukogluu@gmail.com>
2021-01-28 23:02:58 +0300

    new page added & Home page edits

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import LogoutIcon from '../assets/img/sign-out-alt-solid.svg';
 import { auth } from '../firebase';
+import Tasks from './Tasks';
 
 const Home = props => {
     const [user, setUser] = useState('');
@@ -24,12 +25,13 @@ const Home = props => {
 
     return (
         <Container>
-            <Row className="justify-content-between">
+            <Row className="justify-content-between pt-3">
                 <p>Hoşgeldiniz, <b>{user}</b></p>
                 <Link to="/" onClick={userLogout}>
                     <button className="button-logout">Logout<img src={LogoutIcon} alt="Logout" /></button>
                 </Link>
             </Row>
+            <Tasks />
         </Container>
     )
 }
---
+++ src/pages/Tasks.jsx
@@ -0,0 +1,21 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+const Tasks = props => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h6>TODO</h6>
+                </Col>
+                <Col>
+                    <h6>IN PROGRESS</h6>
+                </Col>
+                <Col>
+                    <h6>DONE</h6>                
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Tasks;

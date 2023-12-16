7f68c0f2499a560f1c71168114acda376de7736e
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2023-12-16 22:23:59 +0300

    update project

--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -38,7 +38,7 @@ const TaskList = props => {
                             <Card key={index} border="danger" style={{ cursor: "pointer", marginBottom: "8px" }} onClick={() => setIssueShow(item) || setShowModal(true)}>
                                 <Card.Header>{item.subject}</Card.Header>
                                 <Card.Body>
-                                    <Card.Title className="avatar">{item.assignedUser.charAt(0)}</Card.Title>
+                                    <Card.Title className="avatar">{item.assignedUser.charAt(0).toUpperCase()}</Card.Title>
                                     <Card.Text>{item.description}</Card.Text>
                                 </Card.Body>
                             </Card>
@@ -52,7 +52,7 @@ const TaskList = props => {
                             <Card key={index} border="primary" style={{ cursor: "pointer", marginBottom: "8px" }} onClick={() => setIssueShow(item) || setShowModal(true)}>
                                 <Card.Header>{item.subject}</Card.Header>
                                 <Card.Body>
-                                    <Card.Title className="avatar">{item.assignedUser.charAt(0)}</Card.Title>
+                                    <Card.Title className="avatar">{item.assignedUser.charAt(0).toUpperCase()}</Card.Title>
                                     <Card.Text>{item.description}</Card.Text>
                                 </Card.Body>
                             </Card>
@@ -66,7 +66,7 @@ const TaskList = props => {
                             <Card key={index} border="success" style={{ cursor: "pointer", marginBottom: "8px" }} onClick={() => setIssueShow(item) || setShowModal(true)}>
                                 <Card.Header>{item.subject}</Card.Header>
                                 <Card.Body>
-                                    <Card.Title className="avatar">{item.assignedUser.charAt(0)}</Card.Title>
+                                    <Card.Title className="avatar">{item.assignedUser.charAt(0).toUpperCase()}</Card.Title>
                                     <Card.Text>{item.description}</Card.Text>
                                 </Card.Body>
                             </Card>
